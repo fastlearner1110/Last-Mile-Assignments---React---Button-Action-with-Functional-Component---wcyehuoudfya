f417719c328a88671fd5e94f9229f295b3dd5a18
fastlearner1110 <95926942+fastlearner1110@users.noreply.github.com>
2022-04-23 20:55:53 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -9,7 +9,7 @@ function App() {
   return (
     <div id="main">
       // Do not alter the main div
-      <p id="para">{text}</p>
+    {para && <p id="para">{text}</p>}
       <button onClick={handleClick} id="click">Click</button>
     </div>
   );
