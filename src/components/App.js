f1fdccdc6d2b5eb77b7e3cd3920bc87990f1deeb
fastlearner1110 <95926942+fastlearner1110@users.noreply.github.com>
2022-04-23 20:47:35 +0530

    Add files via upload

--- src/components/App.js
+++ src/components/App.js
@@ -2,9 +2,15 @@ import React, {Component, useState} from "react";
 import "./../styles/App.css";
 
 function App() {
+  let [text,setText]=useState("");
+  function handleClick(){
+     setText("Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy");
+  }
   return (
     <div id="main">
       // Do not alter the main div
+      <p id="para">{text}</p>
+      <button onClick={handleClick} id="click">Click</button>
     </div>
   );
 }
